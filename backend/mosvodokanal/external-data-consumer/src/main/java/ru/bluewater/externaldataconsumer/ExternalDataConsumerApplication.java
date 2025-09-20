1c8e9cd66d13725553dd1a45b447cbadce41982a
optional <pythonuntermensh@gmail.com>
2025-09-21 01:11:35 +0400

    Backend init.

--- backend/mosvodokanal/external-data-consumer/src/main/java/ru/bluewater/externaldataconsumer/ExternalDataConsumerApplication.java
+++ backend/mosvodokanal/external-data-consumer/src/main/java/ru/bluewater/externaldataconsumer/ExternalDataConsumerApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExternalDataConsumerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ExternalDataConsumerApplication.class, args);
     }
-
 }
