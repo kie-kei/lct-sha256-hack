dafe6df519c5a6997b3ca97139c98b36948cc6ed
sklnrr <artemiyilin765@gmail.com>
2025-09-21 22:11:35 +0400

    сделан jwt фильтр для каждого запроса

---
+++ backend/mosvodokanal/vodokanal-data-service/src/main/java/ru/bluewater/vodokanaldataservice/configuration/security/JwtUserDetails.java
@@ -0,0 +1,33 @@
+package ru.bluewater.vodokanaldataservice.configuration.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class JwtUserDetails implements UserDetails {
+
+    private String username;
+    private String fullName;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+}
---
+++ backend/mosvodokanal/vodokanal-data-service/src/main/java/ru/bluewater/vodokanaldataservice/util/JwtUtils.java
@@ -0,0 +1,30 @@
+package ru.bluewater.vodokanaldataservice.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component
+public class JwtUtils {
+    public String extractUsername(String token) {
+        return extractClaim(token, claims -> claims.get("preferred_username", String.class));
+    }
+
+    public String extractFullName(String token) {
+        return extractClaim(token, claims -> claims.get("fullname", String.class));
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+}
